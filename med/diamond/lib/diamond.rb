12f581d89f1f6f31f799a053c5ae7c257cacde39
MASisserson <masisserson@gmail.com>
2021-06-04 09:39:15 -0400

    Fixed a but at line 12 of

--- med/diamond/lib/diamond.rb
+++ med/diamond/lib/diamond.rb
@@ -2,14 +2,14 @@ class Diamond
   def self.make_diamond(letter)
     alphabet_arr = construct_alphabet_arr(letter)
 
+    diamond_height = alphabet_arr.size
     outer_spaces = alphabet_arr.uniq.size - 1
     inner_spaces = 1
-    tot_lines = alphabet_arr.size
-    mid_point = (alphabet_arr.size / 2) + 1
+    mid_point = (diamond_height / 2) + 1
     index = 0
     result = ''
 
-    until index >= tot_lines
+    until index >= diamond_height
       result << create_line(outer_spaces, inner_spaces, alphabet_arr[index])
       index += 1
       if index < mid_point
